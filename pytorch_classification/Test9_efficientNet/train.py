5abb38add43eaab70ba748ea187ad57e8a06e679
LiquidFox <ginyz@foxmail.com>
2021-10-02 16:47:01 +0800

    update from mac code 2

--- pytorch_classification/Test9_efficientNet/train.py
+++ pytorch_classification/Test9_efficientNet/train.py
@@ -8,7 +8,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 import torch.optim.lr_scheduler as lr_scheduler
 
-from model import efficientnet_b0 as create_model
+from model import efficientnet_b7 as create_model
 from my_dataset import MyDataSet
 from utils import read_split_data, train_one_epoch, evaluate
 
