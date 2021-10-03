878aa788ceb8c510bbc3d921b2dc49a06bb51ece
LiquidFox <ginyz@foxmail.com>
2021-10-03 21:28:51 +0800

    update from mac code 10

--- myselfModel/efficientNet4/predict.py
+++ myselfModel/efficientNet4/predict.py
@@ -1,15 +1,16 @@
 import os
-import json
-
 import torch
 from PIL import Image
 from torchvision import transforms
-import matplotlib.pyplot as plt
+import numpy as np
+from shutil import copyfile
 
 from model import efficientnet_b0 as create_model
 
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-def main():
+
+def filter_image_with_dl(src_name, dst_name):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     img_size = {"B0": 224,
@@ -25,51 +26,52 @@ def main():
     data_transform = transforms.Compose(
         [transforms.Resize(img_size[num_model]),
          transforms.CenterCrop(img_size[num_model]),
-         # transforms.ToTensor(),
+         transforms.ToTensor(),
          transforms.Normalize([0.485], [0.229])])
 
     # load image
-    img_path = "/Users/chenjinsheng/Downloads/XinLong_2013_class/other"
-    # assert os.path.exists(img_path), "file: '{}' dose not exist.".format(img_path)
+    # img_path = "/Users/chenjinsheng/PycharmProjects/qihuiProject/1231"
 
-    supported = [".jpg", ".JPG", ".png", ".PNG"]  # 支持的文件后缀类型
-    image_paths = [os.path.join(img_path, i) for i in os.listdir(img_path)
-                   if os.path.splitext(i)[-1] in supported]
-    # img = Image.open(img_path)
-    # plt.imshow(img)
-    # [N, C, H, W]
-    # img = data_transform(img)
-    # expand batch dimension
-    # img = torch.unsqueeze(img, dim=0)
-
-    # read class_indict
-    json_path = './class_indices.json'
-    assert os.path.exists(json_path), "file: '{}' dose not exist.".format(json_path)
-
-    json_file = open(json_path, "r")
-    class_indict = json.load(json_file)
-
-    # create model
     model = create_model(num_classes=2).to(device)
     # load model weights
-    model_weight_path = "./model-29.pth"
-    model.load_state_dict(torch.load(model_weight_path, map_location=device))
+    # model_weight_path = "../efficientNet/efficientnetb0.pth"
+    # model.load_state_dict(torch.load(model_weight_path, map_location=device))
     model.eval()
 
-    for img_path in image_paths:
-        img = Image.open(img_path)
-        img = data_transform(img)
-        img = torch.unsqueeze(img, dim=0)
-        with torch.no_grad():
-            output = torch.squeeze(model(img.to(device))).cpu()
-            predict = torch.softmax(output, dim=0)
-            predict_cla = torch.argmax(predict).numpy()
-            if(predict_cla == 1):
-                print(img_path)
+    supported = [".jpg", ".JPG", ".png", ".PNG"]
 
-        # print_res = "class: {}   prob: {:.3}".format(class_indict[str(predict_cla)],
-        #                                              predict[predict_cla].numpy())
-        # print(print_res)
+    if (os.path.exists(dst_name) == False):
+        os.mkdir(dst_name)
+    else:
+        os.rmdir(dst_name)
+        os.mkdir(dst_name)
+    for root, dirs, files in os.walk(src_name):
+        print(root)  # 当前目录路径
+        # print(dirs)  # 当前路径下所有子目录
+        if (len(dirs) > 0):
+            for dir in dirs:
+                os.mkdir(dst_name + os.sep + dir)
+        # print(files)  # 当前路径下所有非目录子文件
+        if (len(files) > 0):
+            dst_path = dst_name + os.sep + root[-4:]
+            image_paths = [os.path.join(root, i) for i in files
+                           if os.path.splitext(i)[-1] in supported]
+            dst_paths = [os.path.join(dst_path, i) for i in files
+                         if os.path.splitext(i)[-1] in supported]
+            img_open = [data_transform(Image.open(path)) for path in image_paths]
+            img_tensor = torch.stack(img_open, dim=0).to(device)
+
+            with torch.no_grad():
+                output = torch.squeeze(model(img_tensor)).cpu()
+                predict = torch.softmax(output, dim=0)
+                predict_cla = torch.argmax(predict, dim=1).numpy()
+                chose_img_index_list = np.argwhere(predict_cla == 0)
+                img_chose = np.array(image_paths)[chose_img_index_list.reshape(-1)]
+                dst_cp_path = np.array(dst_paths)[chose_img_index_list.reshape(-1)]
+                for src, dst in zip(img_chose, dst_cp_path):
+                    copyfile(src, dst)
+
 
 if __name__ == '__main__':
-    main()
+    filter_image_with_dl("/Users/chenjinsheng/PycharmProjects/qihuiProject/1231",
+                         "/Users/chenjinsheng/Downloads/1231")
